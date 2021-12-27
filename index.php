be28d20b8c58fdc713e8705e117b1c2393ec560c
Krzysztof Huczek <krzysztof.huczek@gmail.com>
2021-12-27 17:18:37 +0100

    changed user_settings.html, added gitignore

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 require 'Router.php';
 
 $path = trim($_SERVER['REQUEST_URI'], '/');
-$path = parse_url( $path, PHP_URL_PATH);
+$path = parse_url($path, PHP_URL_PATH);
 
 Router::get('login', 'DefaultController');
 Router::get('register', 'DefaultController');
